2a636e85577e971d4c2fa4fd4b761838d9caf178
Jonas-July <jonas.july@yahoo.co.uk>
2018-01-10 22:21:37 +0100

    Added file

---
+++ ChessMoveChecker.cpp
@@ -0,0 +1,6 @@
+#include "ChessMoveChecker.hpp"
+
+bool checkChessMove(uint8_t startingPosition, uint8_t landingPosition, uint8_t pieceID)
+{
+    return false;
+}
---
+++ ChessMoveChecker.hpp
@@ -0,0 +1,8 @@
+#ifndef CHESSMOVECHECKER_HPP_INCLUDED
+#define CHESSMOVECHECKER_HPP_INCLUDED
+
+#include <iostream>
+
+bool checkChessMove(uint8_t startingPosition, uint8_t landingPosition, uint8_t pieceID);
+
+#endif // CHESSMOVECHECKER_HPP_INCLUDED
